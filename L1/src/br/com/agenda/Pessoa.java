6e19ee9246d6e8d70c49a62ff8d15adc3d1a37a5
Admin <Admin@192.168.15.54>
2020-03-08 16:17:27 -0300

    lista e atualiza

--- L1/src/br/com/agenda/Pessoa.java
+++ L1/src/br/com/agenda/Pessoa.java
@@ -1,16 +1,21 @@
 package br.com.agenda;
 
+import br.com.menu.Teclado;
+
 public class Pessoa {
 	private String nome;
 	private String telefone;
 	private String datanasc;
 	private String genero;
+	private int cod;
+	Teclado teclado = new Teclado();
 
-	public Pessoa(String nome, String telefone, String datanasc, String genero) {
+	public Pessoa(String nome, String telefone, String datanasc, String genero, int cod) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.datanasc = datanasc;
 		this.genero = genero;
+		this.cod = cod;
 	}
 
 	public String getNome() {
@@ -44,11 +49,34 @@ public class Pessoa {
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
 	
 	public void imprimir() {
+		System.out.println("Codigo: " + cod);
 		System.out.println("Nome: " + nome);
 		System.out.println("Telefone: " + telefone);
 		System.out.println("Data de nascimento: " + datanasc);
 		System.out.println("Genero: " + genero);
+		System.out.println("\n");
+	}	
+		
+	public void atualizaNome(String nome) {
+		this.nome = teclado.texto();
+	}
+	public void atualizaFone(String telefone) {
+		this.telefone = teclado.texto();
+	}
+	public void atualizaNasc(String datanasc) {
+		this.datanasc = teclado.texto();
+	}
+	public void atualizaGen(String genero) {
+		this.genero = teclado.texto();
 	}
 }
+
--- L1/src/br/com/app/App.java
+++ L1/src/br/com/app/App.java
@@ -7,6 +7,7 @@ import br.com.menu.*;
 public class App {
 	public static void main(String[] args) {
 		String n,tel,data,g;
+		int cod = 0, codt, info;
 		Menu menu = new Menu();
 		Agenda agenda = new Agenda();	
 		int op = 1000;
@@ -18,20 +19,69 @@ public class App {
 			case 1: //Adicionar tem que arrumar ainda
 				System.out.println("Defina o nome.");
 				n = teclado.texto();
+				n = teclado.texto();
 				System.out.println("Defina o telefone.");
 				tel = teclado.texto();
 				System.out.println("Defina a data de nascimento.");
 				data = teclado.texto();
 				System.out.println("Defina o genero (M/F).");
 				g = teclado.texto();
-				Pessoa p = new Pessoa(n, tel, data, g);
+				Pessoa p = new Pessoa(n, tel, data, g, cod);
 				agenda.clientes.add(p);
+				cod = cod + 1;
 				break;
 			case 2: // imprimir/listar
 				for (Pessoa x: agenda.clientes) {
-					
+					x.imprimir();
 				}
+				break;
 			case 3: // Alterar
+				System.out.println("Qual o codigo da pessoa que deseja alterar?");
+				codt = teclado.opcao();
+				System.out.println("Qual informação deseja alterar (1 = nome/2 = tel/3 = datanasc/4 = genero/ 0 = sair)");
+				info = teclado.opcao();
+				switch (info) {
+				case 1:
+					for (Pessoa x: agenda.clientes) {
+						if (x.getCod() == codt) {
+							System.out.println("Novo nome");
+							n = teclado.texto();
+							x.atualizaNome(n);
+						}
+					}
+					break;
+				case 2:
+					for (Pessoa x: agenda.clientes) {
+						if (x.getCod() == codt) {
+							System.out.println("Novo telefone");
+							tel = teclado.texto();
+							x.atualizaFone(tel);
+						}
+					}
+					break;
+				case 3:
+					for (Pessoa x: agenda.clientes) {
+						if (x.getCod() == codt) {
+							System.out.println("Nova data nasc");
+							data = teclado.texto();
+							x.atualizaNasc(data);
+						}
+					}
+					break;
+				case 4:
+					for (Pessoa x: agenda.clientes) {
+						if (x.getCod() == codt) {
+							System.out.println("Novo genero");
+							g = teclado.texto();
+							x.atualizaGen(g);
+						}
+					}
+					break;
+				case 0:
+					continue;
+				default:
+					continue;
+				}
 			case 4: // remover
 			case 0: //Sair
 				continue;
